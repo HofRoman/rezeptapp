54bd537d7c1afcf545bfb57d26a832197e2088cd
Treyger <roman.treyger@accenture.com>
2025-08-19 15:29:58 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/dev/roman/rezeptapp/RezeptappApplication.java
@@ -0,0 +1,13 @@
+package dev.roman.rezeptapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezeptappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezeptappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/roman/rezeptapp/RezeptappApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roman.rezeptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezeptappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
